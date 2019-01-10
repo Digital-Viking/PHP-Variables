0e7a5381eb66ea3fa1e8411d698e16664a302c81
David Weston Webb <westwebb@gmail.com>
2019-01-10 09:56:41 -0500

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$name = 'David';
+$number = 100;
+$Number_List = 100.5;
+// DON'T DO THIS: $num-ber = 400;
+// DON'T DO THIS: $0number = 500;
+
+$NUMBER = 100;
+// THIS IS A DIFFERENT VARIABLE THAN THE ONE ABOVE: $NUMBeR = 100;
+
+echo $name . " " . $NUMBER;
+// using . means you are using concatenation;
+
+$name = "<h1> HELLO </h1>;
+// you can put html inside a variable too!;
+
+echo $name;
+
+?>
+
+</body>
